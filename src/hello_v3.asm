ce72776ede7a440826420c954d7a0717dc2c3ecf
Mário Nunes <mariogar1979@gmail.com>
2016-10-18 02:11:31 -0200

    Adição de alguns exemplos do hello World, em Nasm e Fasm

---
+++ src/hello_v3.asm
@@ -0,0 +1,18 @@
+section .text
+
+global _start
+
+_start: 
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, msg
+	mov edx, len
+	int 0x80
+
+	mov eax, 1
+	xor ebx, ebx
+	int 0x80
+
+section .data
+	msg: db	'Ola Mundo!', 10,13 
+	len equ $ - msg
